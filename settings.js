42a28335b1843eabf0087f9e70ae07a212617eac
thankgodvictor09091 <davidsimon12321@gmail.com>
2025-09-10 08:20:14 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
+  author: '‎',THANKGOD
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Professor', // DAVE
+  ownerNumber: '919876543210', //2347032416872
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "PUBLIC",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
